16be508eb0e058bc2975f0584fecea498eeb86ad
Imabeaver <66724720+Imabeaver@users.noreply.github.com>
2020-12-10 20:23:00 +0100

    Update final.m

--- final.m
+++ final.m
@@ -8,16 +8,22 @@ ring = v.ring;
 %generate at element
 bb=atbaselem('BeambeamKick','bb_kick');
 
-n_turns = 1000;
+n_turns = 100;
 rin = [1.0e-6 0 0 0 0 0];
 
 fin_x = zeros(1, n_turns);
 fin_xp = zeros(1, n_turns);
+
+xp = zeros(1, n_turns);
+
 figure; hold on
 
 for i = 1:n_turns
     x = rin(1);
     y = rin(3); 
+    
+    fin_x(i) = rin(1);
+    fin_xp(i) = rin(2);
     
     a = py.test.kick_calc(x,y);
     dx = a{1};
@@ -31,14 +37,19 @@ for i = 1:n_turns
     
     rout = ringpass(ring_new, rin', 1);
     rin = rout'; 
-    %plot(rout(1), rout(2), '.'); 
     
-    fin_x(i) = rout(1);
-    fin_xp(i) = rout(2);
-    
+    xp(i) = dx;    
     
 end
 plot(fin_x, fin_xp, '.');
 xlabel('x [m]');
 ylabel('x^{prime} [rad]');
 hold off
+
+figure; 
+[fin_x, indx] = sort(fin_x, 'descend');
+xp = xp(indx);
+plot(fin_x/6.4*1.0e6, xp, '.b');
+xlabel('\sigma_x[m]');
+ylabel('dx[\mu rad]');
+
